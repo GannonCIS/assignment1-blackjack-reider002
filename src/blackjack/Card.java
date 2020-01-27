bc8f9e8a2a89e335367f277a052ef2da3b819139
Ashley Reider <reider002@gannon.edu>
2020-01-27 16:53:32 -0500

    Complete Card class

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -10,5 +10,13 @@ package blackjack;
  * @author gubotdev
  */
 public class Card {
+    public final String RANK;
+    public final String SUIT;
+    //needs constructor with 2 parameters
+    
+    public Card(String rank, String suit){
+        RANK = rank;
+        SUIT = suit;
+    }
     
 }
