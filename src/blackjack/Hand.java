1690c70d6b8de56a4d4e1b67c74e1e31755733f8
Ashley Reider <reider002@gannon.edu>
2020-01-27 17:54:53 -0500

    Complete Hand class structure

--- src/blackjack/Hand.java
+++ src/blackjack/Hand.java
@@ -10,5 +10,28 @@ package blackjack;
  * @author gubotdev
  */
 public class Hand {
-    
+
+    private Card[] myCards = new Card[5];
+    private int numOfCards = 0;
+    private int score = 0;
+
+    public Hand() {
+    }
+    //constructor - default
+
+    public int getNumOfCards() {
+        return numOfCards;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addCard(Card newCard) {
+
+    }
+
+    public void printHand() {
+        //player needds to see their hand
+    }
 }
