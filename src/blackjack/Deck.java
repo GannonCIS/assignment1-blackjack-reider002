723d76f8118dcbd413f75633b89a0d5dc2cd5f61
Ashley Reider <reider002@gannon.edu>
2020-01-27 17:28:27 -0500

    Complete deck class structure

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,5 +10,32 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
-    
+
+    private Card[] myCards = new Card[52];
+    //needs a constructor to be called - new keyword to initialize here
+    private String[] ranks = {};
+    private String[] suits = {};
+    private int nextCard = 0;
+
+    public Deck() {
+
+    }
+
+    private void initDeck() {
+
+    }
+
+    private void shuffle() {
+
+    }
+
+    //print deck is for testing only - delete me later
+    public void printDeck() {
+
+    }
+
+    public Card dealCard() {
+        return null;
+        //returning null so you don't error - do not forget to change this
+    }
 }
